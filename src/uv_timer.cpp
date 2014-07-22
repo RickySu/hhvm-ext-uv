2f6dc479a24b59c7c47a8fc0e728e9e2133e52b3
Ricky Su <ricky@ez2.us>
2014-07-22 14:23:54 +0800

    add uv_timer

---
+++ src/uv_timer.cpp
@@ -0,0 +1,69 @@
+#include "ext.h"
+#include "hphp/runtime/base/thread-init-fini.h"
+
+namespace HPHP {
+
+    typedef struct uv_timer_ext_s:public uv_timer_t{
+        bool start;
+        ObjectData *timer_object_data;
+    } uv_timer_ext_t;
+    
+    static void timer_handle_callback(uv_timer_ext_t *timer_handle) {
+        CallbackResourceData *timer_resource_data = FETCH_RESOURCE(((uv_timer_ext_t *) timer_handle)->timer_object_data, CallbackResourceData, s_uvtimer);
+        vm_call_user_func(timer_resource_data->getCallback(), make_packed_array(((uv_timer_ext_t *) timer_handle)->timer_object_data));
+    }
+    
+    ALWAYS_INLINE void releaseHandle(uv_timer_ext_t *handle) {
+          if(handle->start){
+            handle->start = false;
+            handle->timer_object_data->decRefAndRelease();
+          }
+    }
+    
+    static void HHVM_METHOD(UVTimer, __construct) {
+        Resource resource(NEWOBJ(CallbackResourceData(sizeof(uv_timer_ext_t))));
+        SET_RESOURCE(this_, resource, s_uvtimer);
+        CallbackResourceData *timer_resource_data = FETCH_RESOURCE(this_, CallbackResourceData, s_uvtimer);
+        uv_timer_ext_t *timer_handle = (uv_timer_ext_t*) timer_resource_data->getInternalResourceData();
+        uv_timer_init(uv_default_loop(), timer_handle);
+        timer_handle->start = false;
+    }
+    
+    static int64_t HHVM_METHOD(UVTimer, start, const Variant &timer_cb, int64_t start, int64_t repeat) {
+        CallbackResourceData *resource_data = FETCH_RESOURCE(this_, CallbackResourceData, s_uvtimer);
+        uv_timer_ext_t *timer_handle = (uv_timer_ext_t *) resource_data->getInternalResourceData();
+        resource_data->setCallback(timer_cb);
+        int64_t ret = uv_timer_start(timer_handle, (uv_timer_cb) timer_handle_callback, start, repeat);
+        if(ret == 0){
+            timer_handle->start = true;
+            timer_handle->timer_object_data = this_.get();
+            timer_handle->timer_object_data->incRefCount();
+        }
+        return ret;
+    }
+    
+    static int64_t HHVM_METHOD(UVTimer, stop) {
+        CallbackResourceData *resource_data = FETCH_RESOURCE(this_, CallbackResourceData, s_uvtimer);
+        uv_timer_ext_t *timer_handle = (uv_timer_ext_t *) resource_data->getInternalResourceData();
+        int64_t ret = 0;
+        if(timer_handle->start){
+            ret = uv_timer_stop((uv_timer_t *) timer_handle);
+            releaseHandle(timer_handle);
+        }
+        return ret;
+    }    
+    
+    static void HHVM_METHOD(UVTimer, __destruct) {
+        CallbackResourceData *resource_data = FETCH_RESOURCE(this_, CallbackResourceData, s_uvtimer);
+        uv_timer_ext_t *timer_handle = (uv_timer_ext_t *) resource_data->getInternalResourceData();
+        uv_unref((uv_handle_t *) timer_handle);
+        releaseHandle(timer_handle);
+    }
+    
+    void uvExtension::_initUVTimerClass() {
+        HHVM_ME(UVTimer, __construct);
+        HHVM_ME(UVTimer, __destruct);        
+        HHVM_ME(UVTimer, start);
+        HHVM_ME(UVTimer, stop);
+    }
+}
---
+++ src/uv_timer.php
@@ -0,0 +1,9 @@
+<?hh
+class UVTimer
+{
+    private ?resource $_rs = null;
+    <<__Native>> function __construct():void;
+    <<__Native>> function __destruct():void;
+    <<__Native>> function start(mixed $callbck, int $start, int $repeat = 0): int;
+    <<__Native>> function stop(): int;
+}
