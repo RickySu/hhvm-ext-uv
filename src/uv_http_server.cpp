28797181ccef7d81178e86c0ad929e3a6a07dd38
Ricky Su <ricky@ez2.us>
2014-07-28 15:26:50 +0800

    add route match

--- src/uv_http_server.cpp
+++ src/uv_http_server.cpp
@@ -3,21 +3,43 @@
 
 namespace HPHP {
     
-    static void HHVM_METHOD(UVHttpServer, __construct) {
-        Resource resource(NEWOBJ(R3ResourceData(10)));
-        SET_RESOURCE(this_, resource, s_uvhttpserver);
-//        r3_tree_compile(r3_node->n, NULL);        
-//        r3_tree_dump(r3_node->n, 0);
-    }
-    
-    static void HHVM_METHOD(UVHttpServer, _R3Compile){        
-        R3ResourceData *resource = FETCH_RESOURCE(this_, R3ResourceData, s_uvhttpserver);
-        node *n = resource->getR3ResourceData();
-        r3_tree_compile(n, NULL);
+    static Array HHVM_METHOD(UVHttpServer, _R3Match, const Array &routes, const String &uri, int64_t method) {        
+        Array ret;
+        node *n = r3_tree_create(routes.size());
+        for (ArrayIter iter(routes); iter; ++iter) {
+            Variant key(iter.first());
+            String pattern = routes.rvalAt(key).toArray().rvalAt(0).toString();
+            int64_t idx = key.toInt64Val();
+            if(r3_tree_insert_routel(n, -1, pattern.c_str(), pattern.size(), (void *) idx) == NULL) {
+                r3_tree_free(n);
+                return ret;
+            }
+        }
+        
+        if(r3_tree_compile(n, NULL) != 0) {
+            r3_tree_free(n);
+            return ret;        
+        }
+        
+        match_entry * entry = match_entry_create(uri.c_str());        
+        entry->request_method = method;
+        route *matched_route = r3_tree_match_route(n, entry);
+                
+        if(matched_route != NULL) {
+            Array params;
+            int64_t matched = (int64_t) matched_route->data;
+            for(int i=0;i<entry->vars->len;i++){
+                params.append(Variant(entry->vars->tokens[i]));
+            }
+            ret = make_packed_array(matched, params);
+        }
+        
+        match_entry_free(entry);
+        r3_tree_free(n);
+        return ret;
     }
     
     void uvExtension::_initUVHttpServerClass() {
-        HHVM_ME(UVHttpServer, __construct);
-        HHVM_ME(UVHttpServer, _R3Compile);        
+        HHVM_ME(UVHttpServer, _R3Match);
     }
 }
--- src/uv_http_server.php
+++ src/uv_http_server.php
@@ -1,36 +1,79 @@
 <?hh
 class UVHttpServer
 {
-    private resource $_rs = null;
-    private ?UVTcp $server = null;
-    protected array $callbacks = [];
-    private ?string $host = '0.0.0.0';
-    private int $port = 80;
-
-    <<__Native>>function __construct():void;
-    <<__Native>>private function _R3Compile():void;
+    const METHOD_GET = 2;
+    const METHOD_POST = 4;
+    const METHOD_PUT = 8;
+    const METHOD_DELETE = 16;
+    const METHOD_PATCH = 32;
+    const METHOD_HEAD = 64;
+    const METHOD_OPTIONS = 128;
     
-    function listen(string $host, int $port):UVHttpServer
+    private ?UVTcp $server = null;
+    protected array $routes = [];
+    protected ?mixed $defaultCallback = null;
+
+    <<__Native>>private function _R3Match(array $routes, string $uri, int $method):array;
+    
+    function __construct(string $host, int $port): void
     {
-        $this->host = $host;
-        $this->port = $port;    
+        $this->defaultCallback = function(UVHttpCLient $client){
+            $client->sendReply('Page not found.', 404);
+            $client->setCloseOnBufferEmpty();
+        };
+        $this->server = new UVTcp();
+        return $this->server->listen($host, $port, function($server){
+            new UVHttpClient($server->accept(), function(UVHttpCLient $client){
+                $result = $this->_R3Match($this->routes, $client->getRequest()['request']['uri'], $this->convertMethod([$client->getRequest()['request']['method']]));
+                if($result){
+                    call_user_func_array($this->routes[$result[0]][2], array_merge(array($client), $result[1]));
+                    return;
+                }
+                ($this->defaultCallback)($client);
+            });
+        });        
+    }
+    
+    protected function convertMethod(?array<string> $allowMethod):int
+    {
+        $allowMethodPacked = 0;
+        if(is_array($allowMethod)){
+            foreach($allowMethod as $method){
+                switch(strtoupper($method)){
+                    case 'GET':
+                        $allowMethodPacked|=self::METHOD_GET;
+                        break;
+                    case 'POST':
+                        $allowMethodPacked|=self::METHOD_POST;
+                        break;                                            
+                    case 'PUT':
+                        $allowMethodPacked|=self::METHOD_PUT;
+                        break;                    
+                    case 'DELETE':
+                        $allowMethodPacked|=self::METHOD_DELETE;
+                        break;                    
+                    case 'PATCH':
+                        $allowMethodPacked|=self::METHOD_PATCH;
+                        break;                    
+                    case 'HEAD':
+                        $allowMethodPacked|=self::METHOD_HEAD;
+                        break;
+                    case 'OPTIONS':
+                        $allowMethodPacked|=self::METHOD_OPTIONS;
+                        break;
+                }
+            }
+        }
+        return $allowMethodPacked;
     }
     
     function onRequest(string $pattern, mixed $callback, ?array<string> $allowMethod = null):UVHttpServer
     {    
-        $this->callbacks[] = $callback;
-    }
-    
-    function start():int
-    {
-        $this->_R3Compile();    
-        $this->server = new UVTcp();
-        return $this->server->listen($this->host, $this->port, function($server){
-            new UVHttpClient($server->accept(), function(UVHttpCLient $client){
-                ($this->callbacks[0])($client);
-            });
-        });    
-
+        $this->routes[] = array(
+            $pattern,            
+            $this->convertMethod($allowMethod),            
+            $callback,
+        );
     }
     
 }
