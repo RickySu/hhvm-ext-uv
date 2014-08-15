5c34f6cf25ab1c307074b08d992dc50601c96e80
Ricky Su <ricky@ez2.us>
2014-08-15 17:12:34 +0800

    update test

--- tests/test_uv_resolver.php
+++ tests/test_uv_resolver.php
@@ -3,12 +3,14 @@ require __DIR__ . '/../test-tools.php';
 $resolver = new UVResolver();
 $nameinfook = false;
 $addrinfook = false;
-$resolver->getnameinfo("127.0.0.1", function($status, $host, $service) use(&$nameinfook){
-    $nameinfook =  "$status, $host, $service" == "0, localhost, 0";
+$hostexpect = gethostbyaddr("127.0.0.1");
+$resolver->getnameinfo("127.0.0.1", function($status, $host, $service) use(&$nameinfook, $hostexpect){
+    $nameinfook =  "$status, $host, $service" == "0, $hostexpect, 0";
 });
 
-$resolver->getaddrinfo("localhost", null,function($status, $host) use(&$addrinfook){
-    $addrinfook = "$status, $host" == "0, 127.0.0.1";
+$addrexpect = gethostbyname($hostexpect);
+$resolver->getaddrinfo("localhost", null,function($status, $addr) use(&$addrinfook, $addrexpect){
+    $addrinfook = "$status, $addr" == "0, $addrexpect";
 });
 UVLoop::defaultLoop()->run();
 True($nameinfook, "getnameinfo");
