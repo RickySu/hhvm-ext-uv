b1ca62588f1bbd9133522c9809d3554b670aeb95
Ricky Su <ricky@ez2.us>
2015-05-29 15:51:15 +0800

    add missig uv_ssl.h

---
+++ src/uv_ssl.h
@@ -0,0 +1,47 @@
+#ifndef UV_SSL_H
+#define UV_SSL_H
+
+#include "uv_tcp.h"
+#include <openssl/ssl.h>
+
+#define SSL_METHOD_SSLV2 0
+#define SSL_METHOD_SSLV3 1
+#define SSL_METHOD_SSLV23 2
+#define SSL_METHOD_TLSV1 3
+#define SSL_METHOD_TLSV1_1 4
+#define SSL_METHOD_TLSV1_2 5
+
+#define REGISTER_UV_SSL_CONSTANT(name) \
+    Native::registerClassConstant<KindOfInt64>(s_uvssl.get(), \
+        makeStaticString(#name), \
+        name)
+
+namespace HPHP
+{
+    typedef struct {
+        const SSL_METHOD *ssl_method;
+        SSL_CTX** ctx;
+        int nctx;
+        SSL* ssl;
+        BIO* read_bio;
+        BIO* write_bio;
+    } ssl_ext_t;
+    
+    typedef struct uv_ssl_ext_s: public uv_tcp_ext_t {
+        ssl_ext_t sslResource;
+        Variant sslHandshakeCallback;
+        Variant sslServerNameCallback;
+    } uv_ssl_ext_t;
+    
+    ALWAYS_INLINE void initSSLHandle(uv_ssl_ext_t *handle)
+    {
+        handle->sslResource = {NULL, NULL, 0, NULL, NULL, NULL};
+        handle->sslHandshakeCallback.setNull();
+        handle->sslServerNameCallback.setNull();        
+    }
+    
+    ALWAYS_INLINE uv_ssl_ext_t *fetchSSLHandle(UVTcpData *data){
+        return (uv_ssl_ext_t *) data->tcp_handle;
+    }    
+}
+#endif
