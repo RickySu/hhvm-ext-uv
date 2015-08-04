ccebe360abf11c6406c46810e858f1b67d5a7edb
Ricky Su <ricky@ez2.us>
2015-08-04 15:45:21 +0800

    add missing uv_loop_data.h

---
+++ src/uv_loop_data.h
@@ -0,0 +1,14 @@
+#ifndef UV_LOOP_DATA_H
+#define UV_LOOP_DATA_H
+
+namespace HPHP {
+    
+    class UVLoopData {
+        public:
+            uv_loop_t *loop = NULL;
+            UVLoopData();
+            ~UVLoopData();
+    };    
+}
+
+#endif
